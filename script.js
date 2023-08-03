c93f4877b0d91df88042e93bd620ee4a7c281c90
Vidya Tiwari <127404171+vidyatiwrai@users.noreply.github.com>
2023-08-03 15:23:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ function swapTheme() {
     appDiv.classList.add('night');
 
     swapButton.classList.remove('button_day');
-    swapButton.classList.add('night');
+    swapButton.classList.add('button_night');
   } else {
     // If current theme is night, switch to day theme
     appDiv.classList.remove('night');
     appDiv.classList.add('day');
 
-    swapButton.classList.remove('night');
+    swapButton.classList.remove('button_night');
     swapButton.classList.add('button_day');
   }
 }
